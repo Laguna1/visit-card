2cb4a58124137351278506c82bb70c62a10363f8
Laguna1 <lagunazoomarket@gmail.com>
2022-01-05 00:38:12 +0200

    Add associations for users, visits

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :created_visits, class_name: 'Visit', foreign_key: 'visitor_who_id'
+  has_many :visitor_tos, through: :created_visits
+
+  has_many :requested_visits, class_name: 'Visit', foreign_key: 'visit_to_id'
+  has_many :visitor_whos, through: :requested_visits
+  validates :email, uniqueness: true
+
 end
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Visit < ApplicationRecord
+  belongs_to :visitor_who, class_name: 'User'
+  belongs_to :visit_to, class_name: 'User'
+
+  validates :problem, presence: true, length: { minimum: 7, maximum: 100 }
 end
